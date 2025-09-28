2e9e8ddf03d60263a45213979469d9b729ffdd0e
Ony <ony@ony.world>
2025-09-29 00:11:22 +0300

    with blink

--- dotfiles/nvim/lua/lsp/init.lua
+++ dotfiles/nvim/lua/lsp/init.lua
@@ -1,8 +1,36 @@
 vim.pack.add({
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/rafamadriz/friendly-snippets" },
+	{ src = "https://github.com/saghen/blink.cmp", version = "v1.7.0" },
 })
 
-vim.lsp.enable("lua_ls")
-vim.lsp.enable("nil_ls")
+local servers = { "lua_ls", "nil_ls" }
+
+vim.lsp.enable(servers)
+
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+		},
+	},
+})
+
+require("blink.cmp").setup({
+	keymap = { preset = "default" },
+	appearance = {
+		nerd_font_variant = "mono",
+	},
+})
+
+for _, server in ipairs(servers) do
+	local capabilities = require("blink.cmp").get_lsp_capabilities()
+	vim.lsp.config(server, {
+		capabilities = capabilities,
+	})
+end
+
 
 require("lsp.mappings")
--- dotfiles/nvim/lua/plugins/kanagawa.lua
+++ dotfiles/nvim/lua/plugins/kanagawa.lua
@@ -1,28 +1,33 @@
-vim.pack.add({"https://github.com/rebelot/kanagawa.nvim"})
+vim.pack.add({ "https://github.com/rebelot/kanagawa.nvim" })
 
 require("kanagawa").setup({
-  transparent = true;
-  background = {
-    dark = "dragon",
-    light = "lotus"
-  },
-  colors = {
-    theme = {
-      all = {
-        ui = {
-           bg_gutter = "none"
-        }
-      }
-    }
-  },
-  overrides = function(colors)
-    -- set floating windows transparent
-    local theme = colors.theme
-      return {
-        NormalFloat = { bg = "none" },
-        FloatBorder = { bg = "none" },
-        FloatTitle = { bg = "none" },
-        NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 }
-      }
-  end,
+	transparent = true,
+	background = {
+		dark = "dragon",
+		light = "lotus",
+	},
+	colors = {
+		theme = {
+			all = {
+				ui = {
+					bg_gutter = "none",
+				},
+			},
+		},
+	},
+	overrides = function(colors)
+		-- set floating windows transparent
+		local theme = colors.theme
+		return {
+			NormalFloat = { bg = "none" },
+			FloatBorder = { bg = "none" },
+			FloatTitle = { bg = "none" },
+			NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+			Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+			PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+			PmenuSbar = { bg = theme.ui.bg_m1 },
+			PmenuThumb = { bg = theme.ui.bg_p2 },
+		}
+	end,
 })
