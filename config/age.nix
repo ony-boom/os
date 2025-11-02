c56575422cadb39b06a367b9a5dcb029f2de60fd
Ony <ony@ony.world>
2025-11-02 10:57:01 +0300

    updated cachix

--- config/age.nix
+++ config/age.nix
@@ -6,5 +6,10 @@
   home = config.users.users."ony".home;
 in {
   age.identityPaths = ["${home}/.ssh/agenix" "${home}/.ssh/id_ed25519"];
-  age.secrets.cachix-auth-token.file = self + /secrets/cachix-auth-token.age;
+  age.secrets.cachix-auth-token = {
+    file = self + /secrets/cachix-auth-token.age;
+    mode = "770";
+    owner = "ony";
+    group = "users";
+  };
 }
