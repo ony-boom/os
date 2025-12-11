404efd411ec93ea4e7255d6000ad5e857c83088e
Ony <ony@ony.world>
2025-12-11 13:16:30 +0300

    since now using DP cable, I can use latest nvidia driver

--- config/services/ollama.nix
+++ config/services/ollama.nix
@@ -4,5 +4,13 @@
       enable = true;
       package = pkgs.ollama-cuda;
     };
+    open-webui = {
+      enable = true;
+      port = 8080;
+
+      environment = {
+        WEBUI_AUTH = "False";
+      };
+    };
   };
 }
