cbf350022b76047cdf4014a4ff63f88a9aa9e9da
Ony <ony@ony.world>
2026-04-06 11:38:39 +0300

    claude code with npm

--- programs/programming.nix
+++ programs/programming.nix
@@ -1,6 +1,5 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     webhook
-    claude-code
   ];
 }
--- programs/wm/default.nix
+++ programs/wm/default.nix
@@ -9,7 +9,7 @@
   };
 
   environment.systemPackages = with pkgs; [
-    swww
+    awww
 
     mako
     libnotify
