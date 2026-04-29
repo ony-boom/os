4ad0fcc8b16ae57af3735a0e7760c9e407fb43e4
Ony <ony@ony.world>
2026-04-29 05:40:08 +0300

    re-enable ollama

--- config/services/ollama.nix
+++ config/services/ollama.nix
@@ -1,18 +1,10 @@
 {pkgs, ...}: let
-  enable = false;
+  enable = true;
 in {
   services = {
     ollama = {
       inherit enable;
       package = pkgs.ollama-cuda;
-    };
-    open-webui = {
-      inherit enable;
-      port = 8080;
-
-      environment = {
-        WEBUI_AUTH = "False";
-      };
     };
   };
 }
