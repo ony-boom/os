b84e09aa023e0ed7f0c271244fda418205ed3011
Ony <ony@ony.world>
2026-05-15 20:50:15 +0300

    cargo stuff

--- programs/programming.nix
+++ programs/programming.nix
@@ -1,12 +1,12 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
-    (pkgs.fenix.complete.withComponents [
-      "cargo"
-      "clippy"
-      "rust-src"
-      "rustc"
-      "rustfmt"
-    ])
+    # (pkgs.fenix.complete.withComponents [
+    #   "cargo"
+    #   "clippy"
+    #   "rust-src"
+    #   "rustc"
+    #   "rustfmt"
+    # ])
 
     rust-analyzer-nightly
   ];
