7b0977c707e393dd2a5bc246925158cd9a50d95d
Ony <ony@ony.world>
2026-05-31 19:44:47 +0300

    more env

--- programs/wm/default.nix
+++ programs/wm/default.nix
@@ -9,8 +9,8 @@
   ];
 
   environment.sessionVariables = {
-    # GBM_BACKEND = "nvidia-drm";
-    # __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    GBM_BACKEND = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
     LIBVA_DRIVER_NAME = "nvidia";
   };
 
