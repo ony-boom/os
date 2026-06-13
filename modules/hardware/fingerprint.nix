baa26dd37a53709b4b7c28140b4f81dcf4e2852e
Ony <onyrakoto27@gmail.com>
2026-06-13 07:03:50 +0300

    sddm enable fprint login

--- modules/hardware/fingerprint.nix
+++ modules/hardware/fingerprint.nix
@@ -3,5 +3,5 @@
   services.fprintd.tod.enable = true;
   services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix; # Goodix driver module
 
-  security.pam.services.login.fprintAuth = false;
+  security.pam.services.login.fprintAuth = true;
 }
