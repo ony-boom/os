d86b4e48db553d362fddcf1cef87c6624354ac93
Ony <onyrakoto27@gmail.com>
2026-06-13 17:54:11 +0300

    removed fenix cache

--- config/nix.nix
+++ config/nix.nix
@@ -2,12 +2,10 @@
   nix.settings = {
     extra-substituters = [
       "https://nix-community.cachix.org"
-      "https://fenix.cachix.org"
       "https://cuda-maintainers.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "fenix.cachix.org-1:ecJhr+RdYEdcVgUkjruiYhjbBloIEGov7bos90cZi0Q="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     ];
     trusted-users = ["root" "ony"];
