7f4f33e460f51de7f884d06154ca2d9bca4ee41f
Ony <onyrakoto27@gmail.com>
2026-07-06 09:52:21 +0300

    dms service seems to be not working anymore

--- dotfiles/dot_config/hypr/modules/autostart.lua
+++ dotfiles/dot_config/hypr/modules/autostart.lua
@@ -4,6 +4,7 @@
 -- dms.lua, not here.
 hl.on("hyprland.start", function()
 	hl.exec_cmd("dms run")
+	hl.exec_cmd("vicinae server")
 	-- gnome-keyring's secrets component is already started by PAM
 	-- (pam_gnome_keyring at SDDM login), so no manual --start is needed here.
 end)
